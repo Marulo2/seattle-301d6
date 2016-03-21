a33523297bb5f2762bff2196adfd2caa7eac1792
Justin Lange <well1912@gmail.com>
2016-03-21 13:25:47 -0700

    Removed TODO, console.logs

--- class-11-spa-routing/pair-assignment/starter-code/scripts/aboutController.js
+++ class-11-spa-routing/pair-assignment/starter-code/scripts/aboutController.js
@@ -1,14 +1,10 @@
 (function(module) {
   var aboutController = {};
 
-  // TODO: Define a function that hides all main section elements, and then reveals just the #about section:
+  // DONE: Define a function that hides all main section elements, and then reveals just the #about section:
   aboutController.index = function() {
       $('.tab-content').hide();
       $('#about').fadeIn();
-
-      console.log('aboutController fired')
-
-    // $('.main-nav .tab:first').click();
   };
 
   module.aboutController = aboutController;
