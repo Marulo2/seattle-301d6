c3317706a9b35ebe5680b9495cf4959cba084eb8
Mars <marshallmiller92@gmail.com>
2016-03-21 13:03:37 -0700

    setup page.js

--- class-11-spa-routing/pair-assignment/starter-code/scripts/aboutController.js
+++ class-11-spa-routing/pair-assignment/starter-code/scripts/aboutController.js
@@ -3,7 +3,12 @@
 
   // TODO: Define a function that hides all main section elements, and then reveals just the #about section:
   aboutController.index = function() {
+      $('.tab-content').hide();
+      $('#about').fadeIn();
 
+      console.log('aboutController fired')
+
+    // $('.main-nav .tab:first').click();
   };
 
   module.aboutController = aboutController;
